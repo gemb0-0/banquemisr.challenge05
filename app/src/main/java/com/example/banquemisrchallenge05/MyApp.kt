cbea109e0faee96275452a1723deb68f7aa9e194
gemb0_0 <agamal00500@gmail.com>
2024-10-28 02:01:19 +0300

    coil caching

--- app/src/main/java/com/example/banquemisrchallenge05/MyApp.kt
+++ app/src/main/java/com/example/banquemisrchallenge05/MyApp.kt
@@ -1,7 +1,35 @@
 package com.example.banquemisrchallenge05
 
 import android.app.Application
+import coil.ImageLoader
+import coil.ImageLoaderFactory
+import coil.disk.DiskCache
+import coil.memory.MemoryCache
+import coil.request.CachePolicy
+import coil.util.DebugLogger
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class MyApp: Application()
+class MyApp : Application(), ImageLoaderFactory {
+
+    //caching for coil
+    override fun newImageLoader(): ImageLoader {
+        return ImageLoader(this).newBuilder()
+            .memoryCachePolicy(CachePolicy.ENABLED)
+            .memoryCache {
+                MemoryCache.Builder(this)
+                    .maxSizePercent(0.25)
+                    .build()
+            }
+            .diskCachePolicy(CachePolicy.ENABLED)
+            .diskCache {
+                DiskCache.Builder()
+                    .maxSizeBytes(1024 * 1024 * 400) // 400megabytes cache
+                    .directory(cacheDir)
+                    .build()
+            }
+            .logger(DebugLogger())
+            .build()
+
+    }
+}
